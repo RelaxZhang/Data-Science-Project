bd69b0e8267c13b1919f154f5e3409e216e2cebf
Chi Zhang <Relaxrelaxzhang1017@gmail.com>
2022-04-25 16:05:40 +1000

    create git and preliminary analysis

---
+++ totpop1000_corr.R
@@ -0,0 +1,10 @@
+# Correlation Calculation
+totpop_1000 <- read.csv(file="totpop_1000.csv")
+corr_matrix <- as.matrix(cor(totpop_1000))
+
+# Extract regions for presentation
+sample_corr <- corr_matrix[c("Latrobe Valley", "Barossa", "Wellington", "Gawler - Two Wells"),
+                           c("Latrobe Valley", "Barossa", "Wellington", "Gawler - Two Wells")]
+
+# Save the correlation matrix as csv for further analysis
+write.csv(data.frame(corr_matrix), paste0("totpop1000_corrmatrix.csv"))
