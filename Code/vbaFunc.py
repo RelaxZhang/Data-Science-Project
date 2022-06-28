5bcf0a5aba0a5e4e2371462dcb29c05cbe015257
Chi Zhang <Relaxrelaxzhang1017@gmail.com>
2022-06-29 03:53:01 +1000

    finish vba read in function setup

--- Code/vbaFunc.py
+++ Code/vbaFunc.py
@@ -125,3 +125,143 @@ def readSATP(intervals, sheet_SmallAreaTotals, numareas):
     
     # Return Small Area Total Population for specific range of years
     return smallAreaTotalPop
+
+'''Function for recording ERP in small area with age & sex information'''
+def readERP(intervals, numareas, numages, sheet_agesex):
+
+    # Create empty array for storing ERP in small area by age and sex
+    infoERP = numpy.zeros((intervals, numareas, 2, numages))
+    infoERP[:] = numpy.nan
+
+    # Collect data
+    for year in range(intervals):
+        row = 3
+        for i in range(numareas):
+            for a in range(numages):
+                row += 1
+                # Females
+                infoERP[year, i, 0, a] = sheet_agesex.cell_value(row, 6 + year)
+                # Males
+                infoERP[year, i, 1, a] = sheet_agesex.cell_value(row, 4 + year)
+    
+    # Return ERP data with given year range(s)
+    return infoERP
+
+'''Function to record TFR information'''
+def readTFR(final, numareas, sheet_fertility):
+
+    # Create empty array for stroing TFR information
+    infoTFR = numpy.zeros((final+1, numareas))
+    infoTFR[:] = numpy.nan
+
+    # Collect TFR values
+    row = 5
+    col = 3
+    for i in range(numareas):
+        row += 1
+        for a in range(final+1):
+            infoTFR[a, i] = sheet_fertility.cell_value(row, col)
+    
+    # Return TFR array
+    return infoTFR
+
+'''Function to read in data for ASFRs and Preliminary ASFRs Model'''
+def readASFR(numareas, sheet_fertility, age_groups):
+
+    # Create empty list and array for storing data
+    ASFR = [None] * age_groups
+    prelimASFR = numpy.zeros((numareas, age_groups))
+    prelimASFR[:] = numpy.nan
+
+    # Collect data for ASFRs Model
+    row = 2258
+    for i in range(age_groups):
+        row += 1
+        ASFR[i] = sheet_fertility.cell_value(row, 1)
+    
+    # Collect data for preliminary ASFRs Model
+    row = 5
+    for i in range(numareas):
+        row += 1
+        if sheet_fertility.cell_value(row, 16) != '':
+            col = 15
+            for a in range(age_groups):
+                col += 1
+                prelimASFR[i, a] = sheet_fertility.cell_value(row, col)
+        else:
+            col = 15
+            for a in range(age_groups):
+                col += 1
+                prelimASFR[i, a] = 0
+    
+    # Return ASFRs and Preliminary ASFRs Model's data
+    return ASFR, prelimASFR
+
+'''Function to collect Life Expectancy at Birth Assumptions, Mprtality Surface Information'''
+def readLEMS(final, numareas, lastage, nLxMS, sheet_mortality):
+    
+    # Create empty array for storing Life expectancy and Mortality Surface
+    eO = numpy.zeros((final + 1, numareas, 2))
+    eO[:] = numpy.nan
+    MS_nLx = numpy.zeros((nLxMS, 2, lastage + 1))
+    MS_nLx[:] = numpy.nan
+    MS_TO = numpy.zeros((nLxMS, 2))
+    MS_TO[:] = numpy.nan
+
+    # Collect Life Expectancy Data
+    row_eO = 5
+    for i in range(numareas):
+        col_eO = 2
+        for a in range(final + 1):
+            col_eO += 1
+            eO[a, i, 0] = sheet_mortality.cell_value(row_eO, col_eO)
+            eO[a, i, 1] = sheet_mortality.cell_value(row_eO, col_eO + 13)
+    
+    # Collect Mortality Surface Data
+    row_MS = 2259
+    for i in range(lastage + 1):
+        row_MS += 1
+        col_MS = 4
+        for a in range(nLxMS):
+            col_MS += 1
+            MS_nLx[a, 0, i] = sheet_mortality.cell_value(row_MS, col_MS)
+            MS_nLx[a, 1, i] = sheet_mortality.cell_value(row_MS + 21, col_MS)
+    
+    col_TO = 4
+    for i in range(nLxMS):
+        col_TO += 1
+        MS_TO[i, 0] = sheet_mortality.cell_value(2278, col_TO)
+        MS_TO[i, 1] = sheet_mortality.cell_value(2299, col_TO)
+    
+    # Return Life Expectancy and Mortality Surface Data
+    return eO, MS_nLx, MS_TO
+
+'''Function of data collection for ASMRs model and Migration Information'''
+def readMASMR(numareas, lastage, sheet_migration):
+    
+    # Create empty list / array for storing data of migration and for ASMRs Model
+    totmig = [None] * numareas
+    modelASMR = numpy.zeros((2, lastage + 1))
+    modelASMR[:] = numpy.nan
+
+    # Collect Migration Data
+    row_mig = 4
+    for i in range(numareas):
+        row_mig += 1
+        # Default Value
+        totmig[i] = sheet_migration.cell_value(row_mig, 3)
+        # User-supplied Value
+        if sheet_migration.cell_value(row_mig, 4) != '':
+            totmig[i] = sheet_migration.cell_value(row_mig, 4)
+    
+    # Collect ASMRs Model Data
+    row_ASMR = 10
+    for i in range(lastage + 1):
+        row_ASMR += 1
+        col = 6
+        for a in range(2):
+            col += 1
+            modelASMR[a, i] = sheet_migration.cell_value(row_ASMR, col)
+    
+    # Return Data of Migration and ASMRs Model
+    return totmig, modelASMR
